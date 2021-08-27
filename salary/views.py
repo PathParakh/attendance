3338070276048fb1116b1c0c815958db5f582b88
Path Parakh <pathparakh@Paths-MacBook-Air.local>
2021-08-27 17:41:40 +0530

    commit

--- salary/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
-# Create your views here.
-def index(request):
-    return render(request, "salary/index.html")
-def user(request):
-    return render(request, "salary/user.html")
