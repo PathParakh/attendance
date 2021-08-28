d7ebdd57a2ca0c53bc24c3b950c4a996877784e9
Path Parakh <pathparakh@Paths-MacBook-Air.local>
2021-08-28 19:04:25 +0530

    table created

--- attendance/settings.py
+++ attendance/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'salary',
+    'attendance',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- salary/models.py
+++ salary/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class staff(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=250)
+    role = models.CharField(max_length=250)
+    salary = models.CharField(max_length=250)
+    address = models.CharField(max_length=250)
+    number = models.CharField(max_length=250)
+    date = models.DateField()
