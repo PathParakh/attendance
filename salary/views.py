239ac32632d673dc94d200410d346caf6e62a73e
Path Parakh <pathparakh@Paths-MacBook-Air.local>
2021-08-28 23:26:52 +0530

    form connected to db

--- salary/views.py
+++ salary/views.py
@@ -1,8 +1,24 @@
+from django.db.models.fields import DateTimeField
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import staff
+import datetime
 # Create your views here.
 def index(request):
     return render(request, "salary/index.html")
 def user(request):
-    return render(request, "salary/user.html")
+    if request.method == "POST" :
+        name = request.POST['name']
+        role = request.POST['role']
+        salary = request.POST['salary']
+        address = request.POST['address']
+        number = request.POST['number']
+        date = datetime.date.today()
+
+        ins = staff(name=name, role=role, salary=salary, address=address, date=date, number=number)
+        ins.save()
+    staffs = staff.objects.all()
+    return render(request, "salary/user.html", {'staff': staffs})
+
+    
